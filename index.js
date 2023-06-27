812e7063c329b01b9ecbd909a40812d8df24a2f6
samuel <samuelkariuki1@student.moringaschool.com>
2023-06-28 00:40:31 +0300

    finished test

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+  const address = 'http://localhost:3000/users';
+  const info = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(info)
+  };
+
+  return fetch(address, config)
+    .then(response => response.json())
+    .then(info => {
+      const id = info.id;
+      document.body.innerHTML += `<p>Created user with id: ${id}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
+
+
+
+
+
+
+
